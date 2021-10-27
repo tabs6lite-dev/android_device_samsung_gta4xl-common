4b77937773f2b5c82d49cfd8ba1767bd1b4bc251
DarkJoker360 <simoespo159@gmail.com>
2021-10-27 05:28:37 -0700

    gta4xl-common: Use Clang 12
    * For some odd reasons everything above clang 12
      is killing boot.
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -103,6 +103,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_ADDITIONAL_FLAGS := HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := r383902b1
 TARGET_KERNEL_SOURCE := kernel/samsung/gta4xl
 
 ## Keymaster
