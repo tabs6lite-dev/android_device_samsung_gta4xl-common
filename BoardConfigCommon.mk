bb391f437067b205f5516959a390aac168214b94
DarkJoker360 <simoespo159@gmail.com>
2023-05-19 15:48:52 +0200

    Revert "gta4xl-common: Compile the kernel without GCC"
    * Breaks compilation.
    
    This reverts commit 9590a968b94575cd3ff26de4628c188508344008.
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -107,7 +107,6 @@ BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_ADDITIONAL_FLAGS := HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_USE_DEFAULT_CLANG := true
-TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/samsung/gta4xl
 
 ## Keymaster
